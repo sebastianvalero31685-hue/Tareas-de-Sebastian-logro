7c9b8535d853d3880321e8245d6273469c701b3c
sebastianvalero31685-hue <sebastianvalero31685@gmail.com>
2026-08-08 12:20:01 -0400

    Create ejercio1.py

---
+++ ejercio1.py
@@ -0,0 +1,9 @@
+# 1️⃣ Suma condicionada
+num1 = int(input("Ingresa el primer número: "))
+num2 = int(input("Ingresa el segundo número: "))
+suma = num1 + num2
+
+if suma > 100:
+    print(f"La suma es {suma} y es mayor a 100.")
+else:
+    print(f"La suma es {suma} y no supera 100.")
